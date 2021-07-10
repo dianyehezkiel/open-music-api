2b4b09b1a70a38bc487fac68672d8399e02bb892
yehezkieldian <dianyehezkiel@gmail.com>
2021-07-10 20:29:03 +0700

    implement users feature

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
+// dotenv and Hapi
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+
+// songs
 const songs = require('./api/songs');
 const SongsService = require('./services/postgresql/SongService');
 const {SongsValidator} = require('./validator/songs');
 
+// users
+const users = require('./api/users');
+const UserService = require('./services/postgresql/UserService');
+const {UsersValidator} = require('./validator/users');
+
 const init = async () => {
   const songsService = new SongsService();
+  const usersService = new UserService();
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -17,13 +26,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: songs,
-    options: {
-      service: songsService,
-      validator: SongsValidator,
+  await server.register([
+    {
+      plugin: songs,
+      options: {
+        service: songsService,
+        validator: SongsValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
