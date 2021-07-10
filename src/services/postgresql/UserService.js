4bb4bf4012e49e79c2391f850ce2bbbe21dc1d21
yehezkieldian <dianyehezkiel@gmail.com>
2021-07-11 01:16:07 +0700

    implement authentications

--- src/services/postgresql/UserService.js
+++ src/services/postgresql/UserService.js
@@ -2,6 +2,7 @@ const {Pool} = require('pg');
 const {nanoid} = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 
 class UsersService {
